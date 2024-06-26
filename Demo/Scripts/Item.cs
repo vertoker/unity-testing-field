165dd630c0237de040db52a067ccc78b50a0c894
vertoker <vertogpro@gmail.com>
2024-06-27 00:43:22 +0500

    update demo

--- Demo/Scripts/Item.cs
+++ Demo/Scripts/Item.cs
@@ -4,19 +4,19 @@ namespace SpatialHashing.Demo
 {
     public class Item : MonoBehaviour
     {
-        [SerializeField] private Material green;
-        [SerializeField] private Material red;
+        [SerializeField] private Material enableMaterial;
+        [SerializeField] private Material disableMaterial;
         [SerializeField] private new MeshRenderer renderer;
 
         public Vector3 Position => transform.position;
 
         public void SetEnable()
         {
-            renderer.material = green;
+            renderer.material = enableMaterial;
         }
         public void SetDisable()
         {
-            renderer.material = red;
+            renderer.material = disableMaterial;
         }
     }
 }
